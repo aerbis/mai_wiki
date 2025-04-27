468e9066eb46df773f281b02e01a3950f12cab5a
Levashkin Sergei <levashkin.leo.ser@gmail.com>
2025-04-27 15:47:15 +0300

    Доработаны интерфейс и логика окна ожидания подтверждения учетной записи

--- resources/views/auth/wait.blade.php
+++ resources/views/auth/wait.blade.php
@@ -1,11 +1,19 @@
 <x-guest-layout>
-    <form method="POST" action="{{ route('register') }}">
+    <form method="POST" action="{{ route('logout') }}">
         @csrf
-        <div class="flex items-center justify-end mt-4">
-            <h3 class="text-white">Дождитесь, пока администратор организации откроет Вам доступ. Вы можете выйти из данной учетной записи, чтобы зайти в другую.</h3>
-            <x-primary-button class="ms-4">
-                Выход
-            </x-primary-button>
+        <div class="flex items-center justify-center mt-4">
+            <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-exclamation-triangle" viewBox="0 0 16 16">
+            <path d="M7.938 2.016A.13.13 0 0 1 8.002 2a.13.13 0 0 1 .063.016.15.15 0 0 1 .054.057l6.857 11.667c.036.06.035.124.002.183a.2.2 0 0 1-.054.06.1.1 0 0 1-.066.017H1.146a.1.1 0 0 1-.066-.017.2.2 0 0 1-.054-.06.18.18 0 0 1 .002-.183L7.884 2.073a.15.15 0 0 1 .054-.057m1.044-.45a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767z"/>
+            <path d="M7.002 12a1 1 0 1 1 2 0 1 1 0 0 1-2 0M7.1 5.995a.905.905 0 1 1 1.8 0l-.35 3.507a.552.552 0 0 1-1.1 0z"/>
+            </svg>
+            <h3 class="ms-4">Дождитесь, пока администратор организации откроет Вам доступ</h3>
+        </div>
+        <div class="flex items-center justify-center mt-4">
+            <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('logout') }}"
+            onclick="event.preventDefault();
+            this.closest('form').submit();">
+                Вы можете выйти из учетной записи и зайти в другую
+            </a>
         </div>
     </form>
 </x-guest-layout>
